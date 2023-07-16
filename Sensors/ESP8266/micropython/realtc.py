b2f1169dce6ad973a66d3a4a3505c2f81bb71076
Bryan Blue <bryanblue@arizona.edu>
2023-07-16 12:27:00 -0700

    black, pep8, pylint and cleanup

--- Sensors/ESP8266/micropython/realtc.py
+++ Sensors/ESP8266/micropython/realtc.py
@@ -1,40 +1,53 @@
+"""
+Biosphere 2 remote sensing project
+AUTHOR: Bryan Blue
+EMAIL: bryanblue@arizona.edu
+STARTED: 2023
+Real Time Clock handling
+HiLetgo DS3231 + AT24C32N
+"""
+
 # RTC module default I2C address is 0x57 (dec 87)
 # address range is 0x50 to 0x57 using solder jumpers
 # https://lastminuteengineers.com/ds3231-rtc-arduino-tutorial/
 
-# HiLetgo DS3231 + AT24C32N
 
 # to set the time on the DS3231 use a tuple as shown here
 # i2c = I2C(sda=machine.Pin(4), scl=machine.Pin(5))
 # d = DS3231(i2c)
-# d.set_time((YY, MM, DD, hh, mm, ss, 0, 0))
+# d.set_time((yyyy, MM, DD, hh, mm, ss, 0, 0))
 # set time to 2023, May, 29, 7 am, 11 minutes, 1 second, NA, NA
 # d.set_time((2023, 05, 29, 7, 11, 1, 0, 0))
 
-from machine import I2C, Pin, RTC
 import time
-from ds3231_gen import *
+from machine import I2C, Pin, RTC
+import ds3231_gen
 
 
-def formattime(t):
+def formattime(in_time):
     """produce a date/time format from tuple
     only minute resolution supported"""
 
-    # YY-MM-DD hh:mm:ss
-    return "{}-{:0>2}-{:0>2} {:0>2}:{:0>2}:{:0>2}".format(
-        t[0], t[1], t[2], t[3], t[4], t[5]
-    )
+    # YYYY-MM-DD hh:mm:ss
+    date = f'{in_time[0]}-{in_time[1]:0>2}-{in_time[2]:0>2}'
+    time = f'{in_time[3]:0>2}:{in_time[4]:0>2}:{in_time[5]:0>2}'
+    
+    formatted_time = date + time
+    return formatted_time, date, time
+    # return "{}-{:0>2}-{:0>2} {:0>2}:{:0>2}:{:0>2}".format(
+    #     InTime[0], InTime[1], InTime[2], InTime[3], InTime[4], InTime[5]
+    # )
 
 
 def rtcinit():
     """get the time from the RTC DS3231 board and set the local RTC"""
 
     rtc = RTC()
-    i2c = I2C(sda=machine.Pin(4), scl=machine.Pin(5))
-    d = DS3231(i2c)
-    YY, MM, DD, hh, mm, ss, wday, _ = d.get_time()
+    i2c = I2C(sda=Pin(4), scl=Pin(5))
+    ds3231 = ds3231_gen.DS3231(i2c)
+    YY, MM, DD, hh, mm, ss, wday, _ = ds3231.get_time()
     rtc.datetime((YY, MM, DD, wday, hh, mm, ss, 0))
-    print(f"DS3231 time: {d.get_time()}")
+    print(f"DS3231 time: {ds3231.get_time()}")
     print(f"local time: {formattime(time.localtime())}")
 
 
@@ -68,7 +81,8 @@ def rtcinit():
 # try:
 #     print(f"Local time before NTP: {str(time.localtime())}")
 #     ntptime.settime()
-#     print(f"Local time after NTP: {str(time.localtime(time.time() + conf.UTC_OFFSET))}")
+#     print(f"Local time after NTP:
+# {str(time.localtime(time.time() + conf.UTC_OFFSET))}")
 # except:
 #     print("Error syncing time")
 
