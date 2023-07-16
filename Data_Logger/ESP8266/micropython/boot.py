956369eb55f089821eb47bbc9b203bc9a8ccdf77
Bryan Blue <bryanblue@arizona.edu>
2023-07-16 10:24:17 -0700

    code cleanup

--- Data_Logger/ESP8266/micropython/boot.py
+++ Data_Logger/ESP8266/micropython/boot.py
@@ -5,20 +5,10 @@ EMAIL: bryanblue@arizona.edu
 STARTED: 2023
 """
 
-import gc
-# import time
 import esp
-import conf
-# from machine import Pin
 import realtc
-import sd
-import sys
-from time import sleep
-# import espnowex
-import uerrno  # error trapping and code values
 
 esp.osdebug(None)
-
 
 # pushes first real line of output
 # to the line after the terminal garbage finishes
@@ -28,17 +18,3 @@ print("booting")
 # set the on board RTC to the time from the DS3231
 realtc.rtcinit()
 print("set time")
-
-# attach SD card module and mount the SD card, if one is present
-# TODO error trapping and what to do if there isn't one to use?
-# mnt = '/' + conf.LOG_MOUNT
-# print(f"unmount: {mnt}")
-# sd.closeSD(mnt)
-# print(f"mount: {mnt}")
-# try:
-#     sd.initSD(mnt)
-# except OSError as e:
-#     print(f"OSError: Connection closed {e}")
-#     sys.exit(0)
-
-# sleep(0.5)
