9c4b56d4b7e98f1571abcf1c8d095485160ec828
Bryan Blue <bryanblue@arizona.edu>
2023-07-27 14:28:30 -0700

    added sys logging for time requests

--- Data_Logger/ESP8266/micropython/main.py
+++ Data_Logger/ESP8266/micropython/main.py
@@ -1,99 +1,105 @@
-import machine
+import sys
 import time
+import machine
 
 import logger
 import conf
 import realtc
 import sd
-# import thermocouple
-import lcd
 
 import espnowex
 
-# set the on board RTC to the time from the DS3231
-realtc.rtcinit()
-print("set time")
-
-print("START DATA LOGGER")
-# status pin for logger, GPIO16/D0
-D0 = machine.Pin(16, machine.Pin.OUT)
-D0.on()
-
-rtc = machine.RTC()
-
-sta, ap = espnowex.wifi_reset()
-esp_con = espnowex.init_esp_connection(sta)
-
-# convert hex into readable mac address
-RAW_MAC = espnowex.get_mac(sta)
-MY_MAC = ':'.join(['{:02x}'.format(b) for b in RAW_MAC])
-# print(f"My MAC:: {MY_MAC}")
-print(f"My MAC addres:: {MY_MAC} raw MAC:: {RAW_MAC}")
-
-logname = '/' + conf.LOG_MOUNT + "/" + conf.LOG_FILENAME
-
 def main():
+    # set the on board RTC to the time from the DS3231
+    realtc.rtcinit()
+    print("set time")
+
+    print("START DATA LOGGER")
+    # status pin for logger, GPIO16/D0
+    D0 = machine.Pin(16, machine.Pin.OUT)
+    D0.on()
+
+    rtc = machine.RTC()
+
+    sta, ap = espnowex.wifi_reset()
+    esp_con = espnowex.init_esp_connection(sta)
+
+    # convert hex into readable mac address
+    RAW_MAC = espnowex.get_mac(sta)
+    MY_MAC = ":".join(["{:02x}".format(b) for b in RAW_MAC]).upper()
+    MY_ID = "-".join(["{:02x}".format(b) for b in RAW_MAC]).upper()
+    print(f"My MAC addres: {MY_MAC} raw: {RAW_MAC}")
+
+    LOGNAME = conf.LOG_MOUNT + '/' + conf.LOG_FILENAME
+    SYSLOG =  conf.LOG_MOUNT + '/' + conf.SYSTEM_LOG
+
     while True:
         print("Data Logger: listen for a message")
-        D0.on() # reset LED off as a visual aid
+        D0.on()  # reset LED off as a visual aid
         host, msg = espnowex.esp_rx(esp_con, 10000)
         if host is not None:
-            str_host = ':'.join(['{:02x}'.format(b) for b in host])
+            str_host = ":".join(["{:02x}".format(b) for b in host])
             # D0.off() # turn on indicate a message was received
         else:
-            msg = b'ERROR' # TODO error should be generic
-        
-        # assumption data is utf-8, if not, it may fail
-        str_msg = msg.decode('utf-8')
+            msg = b"ERROR"  # TODO error should be generic
 
-        if msg == b'get_time':
-            D0.off() # turn on indicate a message was received
+        # assumption data is utf-8, if not, it may fail
+        str_msg = msg.decode("utf-8")
+
+        if msg == b"get_time":
+            D0.off()  # turn on indicate a message was received
             print(f"Data Logger: {host}, {str_host} requested the time")
-            time.sleep(0.1) # let other side get ready
+
+            # TODO turn into function
+            tx_mac = ":".join(["{:02x}".format(b) for b in host]).upper()
+            sys_msg = f"Time requested by: {tx_mac}"
+            logger.write_log(SYSLOG, sys_msg)
+
+            time.sleep(0.1)  # let other side get ready
             # receiver blocked until time is received
             espnowex.esp_tx(host, esp_con, str(rtc.datetime()))
-            D0.on() # turn led off, finished rquest
-            print("Data Logger: time sent")
-        elif msg == b'ERROR': # TODO generic trap
-            print("No Messages") # normally this is a timeout, just continue
+
+            # TODO turn into function (see above)
+            tx_mac = ":".join(["{:02x}".format(b) for b in host]).upper()
+            sys_msg = f"Time sent to: {tx_mac}"
+            print(sys_msg)
+            logger.write_log(SYSLOG, sys_msg)
+
+            D0.on()  # turn led off, finished rquest
+        elif msg == b"ERROR":  # TODO generic trap
+            print("No Messages")  # normally this is a timeout, just continue
         else:
             try:
-                D0.off() # turn on indicate a message was received
-                logger.write_log(logname, str_host + ',' + str_msg)
-                D0.on() # turn off led
+                D0.off()  # turn on indicate a message was received
+                # it is assumed the date/time and record number are part of str_msg
+                MsgHost = "-".join(["{:02x}".format(b) for b in host]).upper()
+                log_name = "/".join(
+                    conf.LOG_MOUNT, MsgHost
+                )  # each device has it's own log
+                logger.write_log(log_name, str_host + "," + str_msg)
+                D0.on()  # turn off led
             except OSError as e:
-                D0.off() # turn LED off as a visual aid for error
+                D0.off()  # turn LED off as a visual aid for error
                 if e.args[0] == uerrno.ETIMEDOUT:  # standard timeout is okay, ignore it
                     print("ETIMEDOUT found")  # timeout is okay, ignore it
                 else:  # general case, continue processing, prevent hanging
                     print(f"-------------- WRITE LOG ERROR: {e}")
-                    sleep(5)
-            
-
-
-    # time.sleep(3)
-
-    # print("dump log contents")
-    # logger.cat_log(logname)
-
-
-
-
+                    time.sleep(5)
 
     # ########### !!! if you don't close it, it will get overwritten
     # when the next PYMAKR update is performed!!!!!!!!!!!
-    sd.closeSD('/' + conf.LOG_MOUNT)
+    sd.closeSD(conf.LOG_MOUNT)
+
 
 if __name__ == "__main__":
     try:
-        print(f'rest code: {machine.reset_cause()}')
+        print(f"rest code: {machine.reset_cause()}")
         main()
     except KeyboardInterrupt as e:
-        print(f'Got ctrl-c {e}')
-        sd.closeSD('/' + conf.LOG_MOUNT)
-        sys.exit() # TODO this falls through and resets???? okay for now
-    finally: 
-        print(f'Fatal error, restarting.  {machine.reset_cause()}')
-        sd.closeSD('/' + conf.LOG_MOUNT)
+        print(f"Got ctrl-c {e}")
+        sd.closeSD(conf.LOG_MOUNT)
+        sys.exit()  # TODO this falls through and resets???? okay for now
+    finally:
+        print(f"Fatal error, restarting.  {machine.reset_cause()}")
+        sd.closeSD(conf.LOG_MOUNT)
         machine.reset()
-
