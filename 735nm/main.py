fd0b95782b36a61566bee4c7f6db8cbf0ea74541
Bryan Blue <bryanblue@arizona.edu>
2023-07-31 15:21:04 -0700

    refactor for new broken out main versions

--- 735nm/main.py
+++ 735nm/main.py
@@ -1,20 +1,15 @@
-import sys
-import time
-from math import isnan
-import machine
 
-# import program configuration
+import machine
 import conf
 import realtc
-import thermocouple
 import espnowex
-import calibrate
+
+# THESE ARE IMPORTED AS NEEDED TO SAVE SPACE
+# CHECK THE BLOCK OF ROLES
+# import calibrate
 # import datalogger_main
 # import BME280_main
-import TRC_main  
-
-
-print("START SENSOR")
+# import TRC_main  
 
 
 def init_device():
@@ -28,75 +23,39 @@ def init_device():
     # MY_MAC = ":".join(["{:02x}".format(b) for b in RAW_MAC])
     print(f"My MAC addres:: {MY_MAC} raw MAC:: {RAW_MAC}")
 
-    # set the time from device designated as TIME
-    retries = 0
-    host = ""
-    
-    peer = conf.peers["TIME"]
-    espnowex.esp_tx(peer, esp_con, "get_time")
-    host, msg = espnowex.esp_rx(esp_con)
-
-    # if a message was not received, loop until a time is received
-    # output the current number of retries
-    while not msg:
-        retries += 1
-        espnowex.esp_tx(peer, esp_con, "get_time")
-        host, msg = espnowex.esp_rx(esp_con)
-        print(f"Get Time: unable to get time from {host} retry # {retries}")
-        time.sleep(3)
-
-    # print(host)
-    str_host = ":".join(["{:02x}".format(b) for b in host])
-    # assumption data is utf-8, if not, it may fail
-    str_msg = msg.decode("utf-8")
-
-    print("\n------------------------")
-    print(f"received a respons from {host} {str_host} of: {msg}")
-    evaltime = eval(msg)
-
-    rtc = machine.RTC()
-    rtc.datetime(evaltime)
-    print(f"The new time is: {realtc.formattime(time.localtime())}")
-    print("------------------------\n")
-    return esp_con, sta, str_host
+    return esp_con, sta, RAW_MAC
 
 
 def main():
-    esp_con, station, str_host = init_device()
+    print("--------START DEVICE--------")
+    esp_con, station, RAW_MAC = init_device()
+    # only get the remote time if you are the time source
+    # TODO change to the TIME entry in conf as a comparison
+    # if conf.MYROLE != "DATALOGGER":
     print(f"time set and my role is {conf.MYROLE}")
-    # use the conf.py file to select the roll of the device
-    # and then call the appropriate startup
-    if conf.MYROLE == "CALIBRATE":
-        print("Calibration Mode (terminal window)")
-        calibrate.calibrate_main()
-    elif conf.MYROLE == "DATALOGGER":
-        # call the data logger main here
+    
+    # # use the conf.py file to select the roll of the device
+    # # and then call the appropriate startup
+    # if conf.MYROLE == "CALIBRATE":
+    #     print("Calibration Mode (terminal window)")
+    #     import calibrate
+    #     calibrate.calibrate_main()
+    # elif conf.MYROLE == "DATALOGGER":
+    if conf.MYROLE == "DATALOGGER":
         print("Data Logger")
-        # datalogger_main.data_looger_main()
-    elif conf.MYROLE == "TRCCONTROL":
-        print("Temperature Relay Controller")
-        TRC_main.trc_main(esp_con, station, str_host)
-        # call the thermocouple and relay control main here
-    elif conf.MYROLE == "THP":
-        # call the temp/humidity/pressure main here
-        print("BME280 Temp/Humidity/Pressure")
-        BME280_main.main()
-
-        
-    # D8 = machine.Pin(15, machine.Pin.OUT)
-    # D8.off()
-
-    # sequence = 1 # record number from the last time the system restarted
-
-    # readings = thermocouple.initReadings(conf.readings)
-    # readings, myReadings = thermocouple.read_thermocouples(readings)
-
-    # # temperature_data = ', '.join([str(value[2]) for value in readings.values()])
-    # temperature_data, internal_data = thermocouple.allReadings(readings)
-    # org_data, org_inter = thermocouple.allReadings(myReadings)
-    # date_time, _, _ = realtc.formattime(time.localtime())
-    # out = ','.join([str(sequence), date_time, temperature_data, internal_data])
-    print("--------THE SCRIPT ENDED--------")
+        import datalogger_main
+        datalogger_main.data_looger_main(esp_con, station, RAW_MAC)
+    # elif conf.MYROLE == "TRCCONTROL":
+    #     # realtc.get_remote_time(esp_con)
+    #     print("Temperature Relay Controller")
+    #     import TRC_main
+    #     TRC_main.trc_main(esp_con, station)
+    #     # call the thermocouple and relay control main here
+    # elif conf.MYROLE == "THP":
+    #     # realtc.get_remote_time(esp_con)
+    #     # call the temp/humidity/pressure main here
+    #     print("BME280 Temp/Humidity/Pressure")
+    #     # BME280_main.main()
 
 
 if __name__ == "__main__":
