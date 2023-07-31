6ce9721496bd32944dfd696e2cc528c1013887ef
Bryan Blue <bryanblue@arizona.edu>
2023-07-31 15:17:20 -0700

    commented out all uncessary functions

--- 735nm/logger.py
+++ 735nm/logger.py
@@ -1,49 +1,44 @@
-import os
+# import os
 import realtc
 import time
 import sd
 import conf
 
-# documentation of system call for total space from root '//'
-# os.statvfs('//')
-# f_bsize − preferred file system block size.
-# f_frsize − fundamental file system block size.
-# f_blocks − total number of blocks in the filesystem.
-# f_bfree − total number of free blocks.
-# f_bavail − free blocks available to non-super user.
-# f_files − total number of file nodes.
-# f_ffree − total number of free file nodes.
-# f_favail − free nodes available to non-super user.
-# f_flag − system dependent.
-# f_namemax − maximum file name length.
+# use "//" to get size from root, on chip memory
+# use "//mnt" for any mount named "mnt" stats
+# Example: os.statvfs('//')
+# f_bsize = 0 # preferred file system block size.
+# f_frsize = 1 # fundamental file system block size.
+# f_blocks = 2 # total number of blocks in the filesystem.
+# f_bfree = 3 # total number of free blocks.
+# f_bavail = 4 # free blocks available to non-super user.
+# f_files = 5 # total number of file nodes.
+# f_ffree = 6 # total number of free file nodes.
+# f_favail = 7 # free nodes available to non-super user.
+# f_flag = 8 # system dependent.
+# f_namemax = 9 # maximum file name length.
 
 
-def get_free_space(mnt):
-    """calculate total ree space, use '//' for total"""
+# def get_storage_free_space(mnt):
+#     """calculate total ree space, use '//' for total"""
 
-    disk = os.statvfs(mnt)
-    size_free = disk[0] * disk[4]
-    return size_free
+#     storage = os.statvfs(mnt)
+#     # block size * blocks available
+#     free = storage[f_bsize] * storage[f_bavail]
+#     return free
 
+# def get_storage_total(mnt):
+#     """calculate total storage available, use '//' for total"""
 
-def get_storage_stats(mnt):
-    """easy way to print common storage stats"""
-
-    print(f"total space: {get_total_space(mnt)}")
-    print(f"free space: {get_free_space(mnt)}")
-
-
-def get_total_space(mnt):
-    """calculate total storage available, use '//' for total"""
-
-    disk = os.statvfs(mnt)
-    total_storage = disk[0] * disk[2]
-    return total_storage
+#     storage = os.statvfs(mnt)
+#     # block size * total blocks available
+#     total_storage = storage[f_bsize] * storage[f_blocks]
+#     return total_storage
 
 
 def write_log(filename, data):
     """write out a CSV record starting with current system"""
-    print(f"-----------storing to mount with filename:{filename}")
+    # print(f"-----------storing to mount with filename:{filename}")
     sd.initSD(conf.LOG_MOUNT)
     with open(filename, "a") as f:
         f.write(f"{realtc.formattime(time.localtime())}, {data}")
@@ -51,8 +46,11 @@ def write_log(filename, data):
     sd.closeSD(conf.LOG_MOUNT)
 
 
-def cat_log(filename):
-    with open(filename, "r") as f:
-        for line in f:
-            print(line.rstrip())
-    print("end of log")
+# def cat_log(filename):
+#     """dump contents of filename to the terminal"""
+#     sd.initSD(conf.LOG_MOUNT)
+#     with open(filename, "r") as f:
+#         for line in f:
+#             print(line.rstrip())
+#     print("end of log")
+#     sd.closeSD(conf.LOG_MOUNT)
