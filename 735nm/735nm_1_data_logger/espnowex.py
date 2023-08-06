aa6d0023378ddda507a110b9f3bd231ad3a3fae6
Bryan Blue <bryanblue@arizona.edu>
2023-08-06 11:52:47 -0700

    moved conf driven file into code base

--- 735nm/735nm_1_data_logger/espnowex.py
+++ 735nm/735nm_1_data_logger/espnowex.py
@@ -1,14 +1,18 @@
+"""ESPNow operations for ESP8266 other microcontrollers may not work with this code"""
 
-
+import time
 import espnow
 import network
-import time
+import conf
 
-import network, time
 
 def wifi_reset():   # Reset wifi to AP_IF off, STA_IF on and disconnected
-    sta = network.WLAN(network.STA_IF); sta.active(False)
-    ap = network.WLAN(network.AP_IF); ap.active(False)
+    """wifi needs turned off so that ESPNow can take over
+    call init_esp_connect() after resetting wifi """
+    sta = network.WLAN(network.STA_IF)
+    sta.active(False)
+    ap = network.WLAN(network.AP_IF)
+    ap.active(False)
     sta.active(True)
     while not sta.active():
         time.sleep(0.1)
@@ -17,54 +21,49 @@ def wifi_reset():   # Reset wifi to AP_IF off, STA_IF on and disconnected
         time.sleep(0.1)
     return sta, ap
 
+
 def init_esp_connection(sta):
     """creates and espnow object, wifi_reset() needs called before this"""
     # create espnow connection
-    e = espnow.ESPNow()
-    e.active(True)
+    esp = espnow.ESPNow()
+    esp.active(True)
 
-    # MAC address of peer's wifi interface
+    # MAC addresses of peers
     # example: b'\x5c\xcf\x7f\xf0\x06\xda'
-    # TODO peers should be in the conf file
-    # peer = b'\x8c\xaa\xb5M\x7f\x18'  # my #2 esp8266
-    # peer = b'\xec\xfa\xbc\xcb\xab\xce' # 1st datalogger
-    peer1 = b'\xc4[\xbe\xe4\xfdq'
-    peer2 = b'\x8c\xaa\xb5M\x7f\x18'
-    e.add_peer(peer1) # register the peer for espnow communication
-    e.add_peer(peer2) # register the peer for espnow communication
+    # values are stored in conf.py
+    # register all define peers
+    # [esp.add_peer(conf.peers[key]) for key in conf.peers.keys()]
+    [esp.add_peer(val) for val in conf.peers['DATA_LOGGER']]
 
-    return e
+    return esp
 
 
-def get_mac(wlan_sta):
-    """ get the MAC address and return it as a binary value
-    change binary to human readable:
-    ':'.join(['{:02x}'.format(b) for b in espnowex.get_mac()])
+def get_mac(sta):
+    """ get the MAC address and return it as a binary abd human readable value
     """
 
-    # TODO add some error handling
-    wlan_mac = wlan_sta.config('mac')
-    
-    return wlan_mac
+    binaryMac = sta.config('mac')
+    # change binary to human readable:
+    # humanMac = ':'.join(['{:02x}'.format(b) for b in binaryMac])
+    return binaryMac
 
 
-def esp_tx(peer, e, msg):
-
-    # TODO add support for TX to multiple peers
-    # # MAC address of peer1's wifi interface exmaple:
-    # # peer1 = b'\xe8\x68\xe7\x4e\xbb\x19'
-    # the receiver MAC address
-    # peer = b'\x8c\xaa\xb5M\x7f\x18'  # my #2 esp8266
-    # peer = b'\xec\xfa\xbc\xcb\xab\xce' # 1st datalogger
-    
+def esp_tx(peer, esp_con, msg):
+    # TODO THIS ONLY WORKS FROM REMOTE SENSORS NOW
+    # MAC address of peer1's wifi interface exmaple:
+    # peer1 = b'\xe8\x68\xe7\x4e\xbb\x19'
     # peer = b'\xc4[\xbe\xe4\xfdq'
 
     try:
-        res = e.send(peer, msg, True)  # transmit data and check receive status
-        if not res:
-            print(f"DATA NOT RECORDED response:{res}")
-        else:
-            print(f"DATA TX SUCCESSFUL response:{res}")
+        # transmit data and check receive status
+        res = esp_con.send(peer, msg, True)  # only one TIME entry should exist
+        print(f"sent to {peer}")
+        print(f"{msg}")
+        # TODO protocol returns false, no need to check, possibly handshake later
+        # if not res:
+        #     print(f"DATA NOT RECORDED response:{res} from {peer}")
+        # else:
+            # print(f"DATA TX SUCCESSFUL reseeeponse:{res}")
 
     except OSError as err:
         if err.args[0] == errno.ETIMEDOUT:  # standard timeout is okay, ignore it
@@ -80,14 +79,13 @@ def esp_rx(esp_con, timeout=1000):
     peers need to be added to the espnow connection"""
 
     # wait for a message to process
-    host, msg = esp_con.recv(timeout) # ms timeout on receive
+    host, msg = esp_con.recv(timeout)  # ms timeout on receive
     # TODO change this to trap for errors, no need to check the msg
-    if msg:
-        if msg == b'get_time':
-            # send time to sender
-            print("host: {host} requested time")
-        else:
-            print(f"received from: {host}, message: {msg}")
-    
+    # if msg:
+    #     if msg == b'get_time':
+    #         print("host: {host} requested time")
+    #     else:
+    #         print(f"received from: {host}")
+    #         print(f"message: {msg}")
+
     return host, msg
-                
