7c6b35d9492bbe2c3172bd680e0df86752ad7a8c
Bryan Blue <bryanblue@arizona.edu>
2023-10-15 08:15:25 -0700

    logger list update, new timing values added

--- 735nm/735nm_TRC/conf.py
+++ 735nm/735nm_TRC/conf.py
@@ -12,7 +12,7 @@ VERSION = "25.90.0"
 # the code executes. The different configurations are shown here
 # MYROLE = "CALIBRATE" # command line callibration
 # MYROLE = "DATALOGGER" # data logger box
-MYROLE = "TRCCONTROL" # multiple thermocouple sensor with relay box
+MYROLE = "TRC" # multiple thermocouple sensor with relay box
 # MYROLE = "THP" # temperatue humidity pressure aspirated sensor
 
 # --------------------
@@ -25,11 +25,6 @@ MYNAME = "ESP8266 MicroPython Temperature Sensor and Temperature Control" # long
 # --------------------
 # WiFi NETWORK CONFIGURATION
 # do not use with ESPNow
-# --BIOSPHERE 2 WiFi Atmospheric Lab--
-# publice IP: 150.135.165.93
-# WAP_SSID = "b2science"
-# WAP_PSWD = ""
-# PORT = 80
 
 # --test direct--
 # WAP_SSID = "MicroPython-e37cfc"
@@ -52,23 +47,17 @@ MYNAME = "ESP8266 MicroPython Temperature Sensor and Temperature Control" # long
 #   CALIBRATE - data logger to store calibration data
 peers = {}
 # remote sensor configuration, connect to all data loggers, pick one for time
-peers["DATA_LOGGER"] = [b'\xc4[\xbe\xe4\xfe\x08', b'\x8c\xaa\xb5M\x7f\x18', b'HU\x19\xdf)\x86']  # list of data loggers
-# peers["TIME"] = [b'\xc4[\xbe\xe4\xfe\x08'] # try to get time from here
-peers["TIME"] = [b'HU\x19\xdf)\x86'] # try to get time from M1
-
-# --------------------
-# SENSOR READINGS
-# AVG_INTERVAL - number of minutes used to calculate and send an average
-# AVG_INTERVAL = 5
+peers["DATA_LOGGER"] = [b'\xc4[\xbe\xe4\xfe\x08', b'\x8c\xaa\xb5M\x7f\x18', b'HU\x19\xdf)\x86', b'\xc4[\xbe\xe5\x03R']  # list of data loggers
+peers["TIME"] = [b'\xc4[\xbe\xe5\x03R'] # try to get time from here M1
 
 # --------------------
 # DATA LOGGER
 # these are configuration locaions for a mount and filename
 # on a MicroSD card. Max Card Size: 32 GB
-LOG_MOUNT = "//log" # must start with "//" the root folder
-CAL_FILENAME = "callibration.dat"
-LOG_FILENAME = "logger.dat" # no leading / depricated
-SYSTEM_LOG = "sys.log" # no leading / depricated
+# LOG_MOUNT = "//log" # must start with "//" the root folder
+# CAL_FILENAME = "callibration.dat"
+# LOG_FILENAME = "logger.dat" # no leading / depricated
+# SYSTEM_LOG = "sys.log" # no leading / depricated
 # --------------------
 
 # --------------------
@@ -91,9 +80,18 @@ TMAX = 50 # 122 F
 # Value must be greater than TMAX !!!
 TMAX_HEATER = 60 # 140 F
 
-# delay in seconds between consecutive readings
-TREAD_INTERVAL = 15
+# --------------------
+# SENSOR READINGS
+# --------------------
+# !!!!!!!! LOG_INTERVAl MUST BE GREATER THAN SAMPLE_INTERVAL !!!!!!!
+# LOG_INTERVAL in minutes
+# SAMPLE_INTERVAL in ms e.g. 15 seconds = 15000 ms
+LOG_INTERVAL = 15 # minutes, larger than SAMPLE_INTERVAL
+SAMPLE_INTERVAL = 15000 # ms
 
+# --------------------
+# 5 THERMOCOUPLE CONFIGURATIONS
+# --------------------
 # assign temperature sensors D0 - D4 locations to a data structure
 # first list element: D0 - D4 correspond to the physical pins on the ESP8266
 # second list element: GPIO number corresponding to D0 - D4
@@ -101,29 +99,28 @@ TREAD_INTERVAL = 15
 # sensor readings are recorded in a dictionary containing lists
 readings = {}
 # requres 1 of each value:
-#   HEATER - heating device temperature
+#   HEAT - heating device temperature
 #   CONTROL - control leaf temperature
-#   TREATMENT - leaf that is being heated
-#   D3, D4 - 2 extra sensors
+#   REFERENCE - reference leaf temperature
+#   TREATMENT - heated leaf temperature
+#   CONTROL_HEAT - control heating pad temperature, not on or heating
 #   Define each dictionary element as a PIN, GPIO, TempValue, SensorID, 0.0
-# EXAMPLE:  readings['HEATER'] = [1, 16, 0.0, 101, 0.0]
+# EXAMPLE:  readings['TREATMENT'] = [1, 16, 0.0, 101, 0.0]
 # key = HEATER, PIN = D0, GPIO 16, initial temp value = 0.0
 # SensorID = a unique ID used for identification of the thermocouple in that position
 # Internal Temperature - cold junction on the amplifier board
-#   See CALLIBRATION below
-readings['HEATER'] = [1, 16, 0.0, 101, 0.0]
+readings['TREATMENT'] = [1, 16, 0.0, 101, 0.0]
 readings['CONTROL'] = [2, 5, 0.0, 102, 0.0]
-readings['TREATMENT'] = [3, 4, 0.0, 103, 0.0]
-# two extra sensor locations, default D3 and D4
-readings['D3'] = [4, 0, 0.0, 104, 0.0]
-readings['D4'] = [5, 2, 0.0, 105, 0.0]
+readings['REFERENCE'] = [3, 4, 0.0, 103, 0.0]
+readings['HEAT'] = [4, 0, 0.0, 104, 0.0]
+readings['CONTROL_HEAT'] = [5, 2, 0.0, 105, 0.0]
 
 # OUTPUT ORDER
 # this controls the 5 temperature sensor readings' output order
 # output will be a CSV with values corresponding to this order
 # readingsOrder = ['TREATMENT', 'CONTROL', 'HEATER', 'D3', 'D4']
 # the values are the "key" values from the readings[key] = []
-readingsOrder = ['HEATER', 'CONTROL', 'TREATMENT', 'D3', 'D4']
+readingsOrder = ['TREATMENT', 'CONTROL', 'REFERENCE', 'HEAT', 'CONTROL_HEAT']
 
 # CALLIBRATION TABLE
 # Each thermocouple must be callibrated
@@ -133,10 +130,10 @@ readingsOrder = ['HEATER', 'CONTROL', 'TREATMENT', 'D3', 'D4']
     # beta0 = -15.35578 - offset
     # beta1 = 1.90714 - slope
     # beta2 = -0.01053 - 2nd order, if needed, set to 0 for linear
-callibrations = {}
-# EXAMPLES ONLY !!!!! these values are not correct
-callibrations['101'] = [1, 28.5, 0.262, 0]
-callibrations['102'] = [2, 98.5, 4.20, 0]
-callibrations['103'] = [3, 22.9, -0.0542, 0]
-callibrations['104'] = [4, -14.1, -2.14, 0]
-callibrations['105'] = [5, 21.3, -0.148, 0]
+# callibrations = {}
+# # EXAMPLES ONLY !!!!! these values are not correct
+# callibrations['101'] = [1, 28.5, 0.262, 0]
+# callibrations['102'] = [2, 98.5, 4.20, 0]
+# callibrations['103'] = [3, 22.9, -0.0542, 0]
+# callibrations['104'] = [4, -14.1, -2.14, 0]
+# callibrations['105'] = [5, 21.3, -0.148, 0]
